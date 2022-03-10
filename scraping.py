c31f642719ee18b7c1ff63502d3cc6cbb5b46865
porpor <porsuchira@outlook.com>
2022-03-10 22:50:14 +0700

    lsit coin

--- scraping.py
+++ scraping.py
@@ -1,2 +1,25 @@
 import requests
-from bs4 import BeautifulSoup 
+import lxml 
+from bs4 import BeautifulSoup
+import pandas as pd
+from IPython.display import display
+url = 'https://www.tradingview.com/markets/cryptocurrencies/prices-all/'
+res = requests.get(url)
+#print(res.text[:200])
+soup = BeautifulSoup(res.text, 'html.parser')
+coin = soup.find_all('a', {'class':'tv-screener__symbol'})
+coinLs = []
+#price = soup.find_all('td', {'class':'tv-data-table__cell tv-screener-table__cell tv-screener-table__cell--big'})
+for c in coin:
+    coinLs.append(c.text)
+
+#print(len(price))
+#for p in range(2, len(price), 6):
+#    print(price[p])
+df=pd.DataFrame({'coin':coinLs})
+display(df)
+
+
+
+
+
